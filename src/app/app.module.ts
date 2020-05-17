d55699d00cd90f87fdbca423f23c77bc206d4af1
akitoshi <akitoshii@gmail.com>
2020-05-18 07:14:28 +0900

    feat(bottom-nav): ボトムのタブナビゲーションの追加（spのみ） #13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent, SidebarComponent],
+  declarations: [AppComponent, SidebarComponent, SidebarComponent, BottomNavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
