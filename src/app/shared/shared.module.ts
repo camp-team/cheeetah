e8fae6b38dd40cd1e872a165aae86c3b4323c9a6
akitoshi <akitoshii@gmail.com>
2020-05-20 21:47:43 +0900

    feat(store-card): swiperの導入 #15

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { StoreCardComponent } from './store-card/store-card.component';
 
 import { MatIconModule } from '@angular/material/icon';
+import { SwiperModule } from 'ngx-swiper-wrapper';
 
 @NgModule({
   declarations: [StoreCardComponent],
-  imports: [CommonModule, SharedRoutingModule, MatIconModule],
+  imports: [CommonModule, SharedRoutingModule, MatIconModule, SwiperModule],
   exports: [StoreCardComponent],
 })
 export class SharedModule {}
--- src/app/shared/store-card/store-card.component.ts
+++ src/app/shared/store-card/store-card.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-store-card',
   templateUrl: './store-card.component.html',
-  styleUrls: ['./store-card.component.scss']
+  styleUrls: ['./store-card.component.scss'],
 })
 export class StoreCardComponent implements OnInit {
+  config: SwiperConfigInterface = {
+    direction: 'horizontal',
+    // navigation: true,
+    // navigation: {
+    //   nextEl: '.swiper-next',
+    //   prevEl: '.swiper-prev',
+    // },
+    // pagination: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    slidesPerView: 1.3,
+    spaceBetween: 16,
+    initialSlide: 0,
+    // loop: true,
+    autoHeight: true,
+  };
 
-  constructor() { }
+  storeSlideId = 1;
+  storeNumbers = [...Array(6)].map((_, i) => i + 1);
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
