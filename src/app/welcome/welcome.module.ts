2e13fa4d23de4f0c14e54cb95cbd38c22009e42f
akitoshi <akitoshii@gmail.com>
2020-05-21 10:50:03 +0900

    feat(style): 見出しスタイルの調整

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { SharedModule } from '../shared/shared.module';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [WelcomeComponent],
-  imports: [CommonModule, WelcomeRoutingModule, SharedModule],
+  imports: [CommonModule, WelcomeRoutingModule, SharedModule, MatIconModule],
 })
 export class WelcomeModule {}
