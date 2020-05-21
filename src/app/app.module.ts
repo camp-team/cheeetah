239fc0788a1dccf75d6c10bd7583a3b94a09307b
akitoshi <akitoshii@gmail.com>
2020-05-21 22:06:53 +0900

    feat: angularfireの設定

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,10 @@ import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 // import { SharedModule } from './shared/shared.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -35,6 +39,9 @@ import { HeaderComponent } from './header/header.component';
     MatButtonModule,
     MatBadgeModule,
     // SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAnalyticsModule,
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCLaXLePSi9n3UR5wrv_hqxUI4SsveHgPM',
+    authDomain: 'ng-cheeetah.firebaseapp.com',
+    databaseURL: 'https://ng-cheeetah.firebaseio.com',
+    projectId: 'ng-cheeetah',
+    storageBucket: 'ng-cheeetah.appspot.com',
+    messagingSenderId: '108836747557',
+    appId: '1:108836747557:web:b2a93bc3454cfbe534c2cc',
+    measurementId: 'G-E4DRFS16BM',
+  },
 };
 
 /*
