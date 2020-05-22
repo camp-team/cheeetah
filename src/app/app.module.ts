7756fb38ece957c024a56da4b58c1c47f81cd199
akitoshi <akitoshii@gmail.com>
2020-05-22 17:25:44 +0900

    feat(mat-menu): sidebar, header にmat-menuを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -40,6 +41,7 @@ import { environment } from '../environments/environment';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAnalyticsModule,
     AngularFirestoreModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
