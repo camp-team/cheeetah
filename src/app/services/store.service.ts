96c276349e1dea7a6c3632710b7c30482d6913d9
akitoshi <akitoshii@gmail.com>
2020-05-24 13:16:00 +0900

    feat(store.service): createStoreの追加

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -3,12 +3,21 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { User } from '@interfaces/user';
 import { Store } from '@interfaces/store';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { firestore } from 'firebase';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StoreService {
-  constructor(private db: AngularFirestore) {}
+  // uid = 'v2mtzA22CafleGVjOt5P';
+
+  constructor(
+    private db: AngularFirestore,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
 
   getUsers(): Observable<User[]> {
     return this.db.collection<User>('users').valueChanges();
@@ -16,5 +25,22 @@ export class StoreService {
 
   getStores(): Observable<Store[]> {
     return this.db.collection<Store>('stores').valueChanges();
+  }
+
+  // getSlides() {
+  //   return this.db.collection<StoreSlide>(`stores/uid/slides`).valueChanges();
+  // }
+
+  createStore(store: Omit<Store, 'storeId' | 'createdAt'>) {
+    const storeId = this.db.createId();
+    this.db
+      .doc(`stores/${storeId}`)
+      .set({ storeId, ...store, createdAt: firestore.Timestamp.now() })
+      .then(() => {
+        this.snackBar.open('ストアを作成しました', null, {
+          duration: 2000,
+        });
+        this.router.navigateByUrl('/');
+      });
   }
 }
